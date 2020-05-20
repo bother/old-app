34eb4881ec3bfef49eaf2f27e682ac9d504836d6
Ali Zahid <ali.zahid@live.com>
2020-05-20 04:02:39 +0400

    Automatic infinite scrolling

--- src/components/posts/list.tsx
+++ src/components/posts/list.tsx
@@ -1,12 +1,11 @@
 import React, { FunctionComponent } from 'react'
-import { FlatList, StyleSheet, Text } from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
 
 import { img_hero_not_found } from '../../assets'
 import { Post } from '../../graphql/types'
-import { colors, layout, typography } from '../../styles'
+import { layout } from '../../styles'
 import { Error } from '../error'
 import { Refresher } from '../refresher'
-import { Touchable } from '../touchable'
 import { PostCard } from './card'
 
 interface Props {
@@ -27,35 +26,20 @@ export const PostList: FunctionComponent<Props> = ({
     ListEmptyComponent={
       <Error image={img_hero_not_found} message="No posts found" />
     }
-    ListFooterComponent={
-      fetchNext ? (
-        <Touchable onPress={fetchNext} style={styles.button}>
-          <Text style={styles.more}>More</Text>
-        </Touchable>
-      ) : null
-    }
     contentContainerStyle={styles.content}
     data={posts}
+    onEndReached={fetchNext}
     refreshControl={<Refresher onRefresh={refetch} refreshing={loading} />}
     renderItem={({ item }) => <PostCard post={item} />}
+    style={styles.main}
   />
 )
 
 const styles = StyleSheet.create({
-  button: {
-    alignSelf: 'center',
-    backgroundColor: colors.primaryDark,
-    borderRadius: layout.radius,
-    marginBottom: layout.margin,
-    marginTop: layout.padding,
-    paddingHorizontal: layout.padding * 1.5,
-    paddingVertical: layout.padding
-  },
   content: {
     flexGrow: 1
   },
-  more: {
-    ...typography.small,
-    alignSelf: 'center'
+  main: {
+    paddingBottom: layout.padding
   }
 })
