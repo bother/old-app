ca0e2b3eaa0662af3e0213bced4ce4826f83cffa
Ali Zahid <ali.zahid@live.com>
2020-06-19 11:56:30 +0400

    Sentry added

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ declare module '*.png'
 
 declare module 'react-native-dotenv' {
   const API_URI: string
+  const SENTRY_DSN: string
   const UPLOAD_KEY: string
   const WS_URI: string
 }
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import 'react-native-gesture-handler'
 
+import * as Sentry from '@sentry/react-native'
 import { AppRegistry } from 'react-native'
+import { SENTRY_DSN } from 'react-native-dotenv'
 
 import { name } from './app.json'
 import { Bother } from './src'
+
+Sentry.init({
+  dsn: SENTRY_DSN
+})
 
 AppRegistry.registerComponent(name, () => Bother)
