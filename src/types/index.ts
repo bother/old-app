e406e11ad06d152fe8a4fa97abcd731a9c69c9d9
Ali Zahid <ali.zahid@live.com>
2020-06-19 11:46:36 +0400

    Dependencies upgraded

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,19 +2,3 @@ export type Coordinates = {
   latitude: number
   longitude: number
 }
-
-export type FirebaseMessage = {
-  id: string
-  body: string
-  receiver: string
-  sender: string
-  createdAt: string
-}
-
-export type FirebaseThread = {
-  id: string
-  ended: boolean
-  receiver: string
-  sender: string
-  createdAt: string
-}
